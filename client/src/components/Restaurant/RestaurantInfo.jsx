07fefec6927a8909c79fe6ed74445fcb9f5c362a
Daksh2356 <dakshmakhija@gmail.com>
2022-10-18 23:41:54 +0530

    completed restaurant layout page and ImageGrid

---
+++ client/src/components/Restaurant/RestaurantInfo.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import {} from 'react-icons/'
+
+const RestaurantInfo = () => {
+  return <div>RestaurantInfo</div>;
+};
+
+export default RestaurantInfo;
--- client/src/pages/Restaurant.page.jsx
+++ client/src/pages/Restaurant.page.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import RestaurantLayout from "../layouts/Restaurant.Layout";
 
 const Restaurant = () => {
   return (
     <>
-      <h2>Restaurant</h2>
       <Outlet />
     </>
   );
 };
 
-export default RestaurantLayout(Restaurant);
+export default Restaurant;
